361554021cbd13f49b5a4dced02a9cd1e0d7ddbf
Mo'men Alaswad <faberdash55@gmail.com>
2021-12-11 20:04:19 +0200

    Added a Add Review Form.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
 import FeedbackStats from './components/FeedbackStats'
+import FeedbackForm from './components/FeedbackForm'
 import FeedbackData from './data/FeedbackData'
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4()
+    setFeedback([newFeedback, ...feedback])
+  }
 
   const handleDelete = (id) => {
     if (window.confirm('Delete this feedback item?')) {
@@ -17,6 +24,7 @@ function App() {
     <>
       <Header />
       <div className='container'>
+        <FeedbackForm handleAdd={addFeedback} />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={handleDelete} />
       </div>
